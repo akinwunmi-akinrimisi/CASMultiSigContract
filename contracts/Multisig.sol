a8a0ad6968df9710d09c77ece8be1bb1d9f7b08e
akinwunmi-akinrimisi <akinolaakinrimisi@gmail.com>
2024-09-12 08:22:19 +0100

    multisig contract added

---
+++ contracts/Multisig.sol
@@ -0,0 +1,163 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Multisig {
+    uint8 public quorum;
+    uint8 public noOfValidSigners;
+    uint256 public txCount;
+    uint256 public quorumUpdateCount; // Counter for quorum update proposals
+
+    struct Transaction {
+        uint256 id;
+        uint256 amount;
+        address sender;
+        address recipient;
+        bool isCompleted;
+        uint256 timestamp;
+        uint256 noOfApproval;
+        address tokenAddress;
+        address[] transactionSigners;
+    }
+
+
+    struct QuorumUpdate {
+        uint256 id;
+        uint8 newQuorum;
+        bool isCompleted;
+        uint256 noOfApproval;
+        address[] quorumUpdateSigners;
+    }
+
+    mapping(uint256 => QuorumUpdate) public quorumUpdates; // Stores quorum update proposals
+    mapping(address => mapping(uint256 => bool)) hasSignedQuorumUpdate; // Tracks who has signed for quorum update proposals
+
+
+    mapping(address => bool) isValidSigner;
+    mapping(uint => Transaction) transactions; // txId -> Transaction
+    
+    // signer -> transactionId -> bool (checking if an address has signed)
+    mapping(address => mapping(uint256 => bool)) hasSigned;
+
+    constructor(uint8 _quorum, address[] memory _validSigners) {
+        require(_validSigners.length > 1, "few valid signers");
+        require(_quorum > 1, "quorum is too small");
+
+
+        for(uint256 i = 0; i < _validSigners.length; i++) {
+            require(_validSigners[i] != address(0), "zero address not allowed");
+            require(!isValidSigner[_validSigners[i]], "signer already exist");
+            // require(_validSigners[i] == msg.sender); // msg.sender should be the first valid signer
+            isValidSigner[_validSigners[i]] = true;
+        }
+
+        noOfValidSigners = uint8(_validSigners.length);
+
+        if (!isValidSigner[msg.sender]){
+            isValidSigner[msg.sender] = true;
+            noOfValidSigners += 1;
+        }
+
+        require(_quorum <= noOfValidSigners, "quorum greater than valid signers");
+        quorum = _quorum;
+    }
+
+    function transfer(uint256 _amount, address _recipient, address _tokenAddress) external {
+        require(msg.sender != address(0), "address zero found");
+        require(isValidSigner[msg.sender], "invalid signer");
+
+        require(_amount > 0, "can't send zero amount");
+        require(_recipient != address(0), "address zero found");
+        require(_tokenAddress != address(0), "address zero found");
+
+        require(IERC20(_tokenAddress).balanceOf(address(this)) >= _amount, "insufficient funds");
+
+        uint256 _txId = txCount + 1;
+        Transaction storage trx = transactions[_txId];
+        
+        trx.id = _txId;
+        trx.amount = _amount;
+        trx.recipient = _recipient;
+        trx.sender = msg.sender;
+        trx.timestamp = block.timestamp;
+        trx.tokenAddress = _tokenAddress;
+        trx.noOfApproval += 1;
+        trx.transactionSigners.push(msg.sender);
+        hasSigned[msg.sender][_txId] = true;
+
+        txCount += 1;
+    }
+
+    function approveTx(uint8 _txId) external {
+        Transaction storage trx = transactions[_txId];
+
+        require(trx.id != 0, "invalid tx id");
+        
+        require(IERC20(trx.tokenAddress).balanceOf(address(this)) >= trx.amount, "insufficient funds");
+        require(!trx.isCompleted, "transaction already completed");
+        require(trx.noOfApproval < quorum, "approvals already reached");
+
+        // for(uint256 i = 0; i < trx.transactionSigners.length; i++) {
+        //     if(trx.transactionSigners[i] == msg.sender) {
+        //         revert("can't sign twice");
+        //     }
+        // }
+
+        require(isValidSigner[msg.sender], "not a valid signer");
+        require(!hasSigned[msg.sender][_txId], "can't sign twice");
+
+        hasSigned[msg.sender][_txId] = true;
+        trx.noOfApproval += 1;
+        trx.transactionSigners.push(msg.sender);
+
+        if(trx.noOfApproval == quorum) {
+            trx.isCompleted = true;
+            IERC20(trx.tokenAddress).transfer(trx.recipient, trx.amount);
+        }
+    }
+
+
+    function proposeQuorumUpdate(uint8 _newQuorum) external {
+        require(msg.sender != address(0), "address zero found");
+        require(isValidSigner[msg.sender], "invalid signer");
+        require(_newQuorum > 1 && _newQuorum <= noOfValidSigners, "invalid quorum size");
+
+        uint256 _updateId = quorumUpdateCount + 1;
+        QuorumUpdate storage qUpdate = quorumUpdates[_updateId];
+
+        qUpdate.id = _updateId;
+        qUpdate.newQuorum = _newQuorum;
+        qUpdate.noOfApproval += 1;
+        qUpdate.quorumUpdateSigners.push(msg.sender);
+        hasSignedQuorumUpdate[msg.sender][_updateId] = true;
+
+        quorumUpdateCount += 1;
+    }
+
+    function approveQuorumUpdate(uint256 _updateId) external {
+        QuorumUpdate storage qUpdate = quorumUpdates[_updateId];
+
+        require(qUpdate.id != 0, "invalid update id");
+        require(!qUpdate.isCompleted, "quorum update already completed");
+        require(qUpdate.noOfApproval < quorum, "approvals already reached");
+        require(isValidSigner[msg.sender], "not a valid signer");
+        require(!hasSignedQuorumUpdate[msg.sender][_updateId], "can't sign twice");
+
+        hasSignedQuorumUpdate[msg.sender][_updateId] = true;
+        qUpdate.noOfApproval += 1;
+        qUpdate.quorumUpdateSigners.push(msg.sender);
+
+        if (qUpdate.noOfApproval == quorum) {
+            qUpdate.isCompleted = true;
+            quorum = qUpdate.newQuorum;
+        }
+    }
+
+}
+
+
+
+// Deployed Addresses
+
+// Web3CXIModule#Web3CXI - 0xb264D760D079EfeC7cCD98F65287458C2a91A26C
+// MultisigModule#Multisig - 0x06e48399d9b9a8159Db4180a9123F81a979f5545
